61cdcbac1c50eb08f892d6664121733799b69b5c
vidushi97 <anandvidushi2@gmail.com>
2020-02-16 22:50:57 +0100

    second commit

--- swagger_server/controllers/default_controller.py
+++ swagger_server/controllers/default_controller.py
@@ -3,6 +3,7 @@ import six
 
 from swagger_server.models.student import Student  # noqa: E501
 from swagger_server import util
+from swagger_server.service import student_service
 
 
 def add_student(body):  # noqa: E501
@@ -17,7 +18,7 @@ def add_student(body):  # noqa: E501
     """
     if connexion.request.is_json:
         body = Student.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+    return student_service.add_student(body)
 
 
 def delete_student(student_id):  # noqa: E501
@@ -30,7 +31,10 @@ def delete_student(student_id):  # noqa: E501
 
     :rtype: Student
     """
-    return 'do some magic!'
+    res = student_service.delete_student(student_id)
+    if res:
+        return res
+    return 'Not Found', 404
 
 
 def get_student_by_id(student_id, subject=None):  # noqa: E501
@@ -45,17 +49,14 @@ def get_student_by_id(student_id, subject=None):  # noqa: E501
 
     :rtype: Student
     """
-    return 'do some magic!'
+    res = student_service.get_student_by_id(student_id, subject=subject)
+    if res:
+        return res
+    return 'Not Found', 404
 
-
-def get_student_by_lastname(last_name):  # noqa: E501
-    """Find student by Last Name
-
-    Returns a single student # noqa: E501
-
-    :param last_name: Last Name
-    :type last_name: str
-
-    :rtype: Student
+def get_student_by_lastname(last_name):
+    """Find student by last name
+       Return a single student
+    return last_name
     """
-    return 'do some magic!'
+    
